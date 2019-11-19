144826d752f2c9b98f14ef1660850b54dd9f5fdb
zhangyanling77 <18783226594@163.com>
2019-11-19 16:19:44 +0800

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql')
+const { CategoryModel, ProductModel } = require('./model.js')
 
 const {
   GraphQLObjectType,
@@ -9,11 +10,11 @@ const {
 }  = graphql
 
 // 模拟数据-商品分类
-const categories = [
-  { id: '1', name: '图书' },
-  { id: '2', name: '数码' },
-  { id: '3', name: '服饰' }
-]
+// const categories = [
+//   { id: '1', name: '图书' },
+//   { id: '2', name: '数码' },
+//   { id: '3', name: '服饰' }
+// ]
 // 商品分类查询对象
 const Category = new GraphQLObjectType({
   name: 'Category',
@@ -24,7 +25,8 @@ const Category = new GraphQLObjectType({
       products: {
         type: new GraphQLList(Product),
         resolve(parent){
-          return products.filter(item => item.category === parent.id)
+          // return products.filter(item => item.category === parent.id)
+          return ProductModel.find({ category: parent.id })
         }
       }
     }
@@ -32,15 +34,15 @@ const Category = new GraphQLObjectType({
 })
 
 // 商品查询
-const products = [
-  { id: '1', name: 'koa从入门到放弃', category: '1' },
-  { id: '2', name: '你不知道的JavaScript', category: '1' },
-  { id: '3', name: '佳能K30相机', category: '2' },
-  { id: '4', name: '艾莱依围巾', category: '3' },
-  { id: '5', name: '伊芙丽羽绒服', category: '3' },
-  { id: '6', name: '微前端架构入门', category: '1' },
-  { id: '7', name: 'iPhone XR', category: '2' }
-]
+// const products = [
+//   { id: '1', name: 'koa从入门到放弃', category: '1' },
+//   { id: '2', name: '你不知道的JavaScript', category: '1' },
+//   { id: '3', name: '佳能K30相机', category: '2' },
+//   { id: '4', name: '艾莱依围巾', category: '3' },
+//   { id: '5', name: '伊芙丽羽绒服', category: '3' },
+//   { id: '6', name: '微前端架构入门', category: '1' },
+//   { id: '7', name: 'iPhone XR', category: '2' }
+// ]
 
 const Product = new GraphQLObjectType({
   name: 'Product',
@@ -51,7 +53,8 @@ const Product = new GraphQLObjectType({
       category: {
         type: Category,
         resolve(parent){
-          return categories.find(item => item.id === parent.category)
+          // return categories.find(item => item.id === parent.category)
+          return CategoryModel.findById(parent.category)
         }
       }
     }
@@ -70,14 +73,16 @@ const RootQuery = new GraphQLObjectType({
         }
       },
       resolve(parent, args){
-        return categories.find(item => item.id === args.id)
+        // return categories.find(item => item.id === args.id)
+        return CategoryModel.findById(args.id)
       }
     },
     getCategories: {
       type: new GraphQLList(Category),
       args: {},
       resolve(parent, args){
-        return categories
+        // return categories
+        return CategoryModel.find()
       }
     },
     getProduct: {
@@ -88,19 +93,59 @@ const RootQuery = new GraphQLObjectType({
         }
       },
       resolve(parent, args){
-        return products.find(item => item.id === args.id)
+        // return products.find(item => item.id === args.id)
+        return ProductModel.findById(args.id)
       }
     },
     getProducts: {
       type: new GraphQLList(Product),
       args: {},
       resolv(parent, args){
-        return products
+        // return products
+        return ProductModel.find()
+      }
+    }
+  }
+})
+
+const RootMutation = new GraphQLObjectType({
+  name: 'RootMutation',
+  fields: {
+    addCategory: {
+      type: Category,
+      args: {
+        name: {
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve(parent, args){
+        // args.id = categories.length + 1
+        // categories.push(args)
+        // return args
+        return CategoryModel.create(args)
+      }
+    },
+    addProduct: {
+      type: Product,
+      args: {
+        name: {
+          type: new GraphQLNonNull(GraphQLString)
+        },
+        category: {
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve(parent, args){
+        // args.id = products.length + 1
+        // products.push(args)
+        // return args
+        return ProductModel.create(args)
       }
     }
   }
 })
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: RootMutation
 })
