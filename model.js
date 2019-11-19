ce6e66f878db0b84b284f8f5df666bfac5fb8f17
zhangyanling77 <18783226594@163.com>
2019-11-19 16:20:35 +0800

    Create model.js

---
+++ model.js
@@ -0,0 +1,37 @@
+let mongoose = require('mongoose')
+let ObjectId = mongoose.Schema.Types.ObjectId
+
+const Schema = mongoose.Schema
+
+const conn = mongoose.createConnection('mongodb://localhost/graphql')
+
+conn.on('open', () => {
+  console.log('数据库连接成功！')
+})
+
+conn.on('error', (error) => {
+  console.log(error)
+})
+
+// 用于定义表结构
+const CategorySchema = new Schema({
+  name: String
+})
+// 增删改查
+const CategoryModel = conn.model('Category', CategorySchema)
+
+const ProductSchema = new Schema({
+  name: String,
+  category: {
+    type: ObjectId, // 外键
+    ref: 'Category'  
+  }
+})
+
+const ProductModel = conn.model('Product', ProductSchema)
+
+
+module.exports = {
+  CategoryModel,
+  ProductModel
+}
