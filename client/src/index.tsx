eca46f9a9d7b42eaadff0314dea708bcb7681bac
zhangyanling77 <18783226594@163.com>
2019-11-20 16:16:31 +0800

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,21 +4,9 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks'
 import App from './App'
 
-// import { gql } from 'apollo-boost'
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
 })
-
-// client.query({
-//   query: gql`
-//     query{
-//       getCategories{
-//         id,
-//         name
-//       }
-//     }
-//   `
-// }).then(result => console.log(result))
 
 ReactDOM.render(
   <ApolloProvider client={client}>
