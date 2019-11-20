dacbd9c770ca713e34fcd7e92b3074a59b13456d
zhangyanling77 <18783226594@163.com>
2019-11-20 10:45:38 +0800

    Update schema.js

--- server/schema.js
+++ server/schema.js
@@ -9,12 +9,6 @@ const {
   GraphQLNonNull
 }  = graphql
 
-// 模拟数据-商品分类
-// const categories = [
-//   { id: '1', name: '图书' },
-//   { id: '2', name: '数码' },
-//   { id: '3', name: '服饰' }
-// ]
 // 商品分类查询对象
 const Category = new GraphQLObjectType({
   name: 'Category',
@@ -25,7 +19,6 @@ const Category = new GraphQLObjectType({
       products: {
         type: new GraphQLList(Product),
         resolve(parent){
-          // return products.filter(item => item.category === parent.id)
           return ProductModel.find({ category: parent.id })
         }
       }
@@ -34,16 +27,6 @@ const Category = new GraphQLObjectType({
 })
 
 // 商品查询
-// const products = [
-//   { id: '1', name: 'koa从入门到放弃', category: '1' },
-//   { id: '2', name: '你不知道的JavaScript', category: '1' },
-//   { id: '3', name: '佳能K30相机', category: '2' },
-//   { id: '4', name: '艾莱依围巾', category: '3' },
-//   { id: '5', name: '伊芙丽羽绒服', category: '3' },
-//   { id: '6', name: '微前端架构入门', category: '1' },
-//   { id: '7', name: 'iPhone XR', category: '2' }
-// ]
-
 const Product = new GraphQLObjectType({
   name: 'Product',
   fields: () => (
@@ -53,7 +36,6 @@ const Product = new GraphQLObjectType({
       category: {
         type: Category,
         resolve(parent){
-          // return categories.find(item => item.id === parent.category)
           return CategoryModel.findById(parent.category)
         }
       }
@@ -73,7 +55,6 @@ const RootQuery = new GraphQLObjectType({
         }
       },
       resolve(parent, args){
-        // return categories.find(item => item.id === args.id)
         return CategoryModel.findById(args.id)
       }
     },
@@ -81,7 +62,6 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(Category),
       args: {},
       resolve(parent, args){
-        // return categories
         return CategoryModel.find()
       }
     },
@@ -93,7 +73,6 @@ const RootQuery = new GraphQLObjectType({
         }
       },
       resolve(parent, args){
-        // return products.find(item => item.id === args.id)
         return ProductModel.findById(args.id)
       }
     },
@@ -101,7 +80,6 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(Product),
       args: {},
       resolv(parent, args){
-        // return products
         return ProductModel.find()
       }
     }
@@ -119,9 +97,6 @@ const RootMutation = new GraphQLObjectType({
         }
       },
       resolve(parent, args){
-        // args.id = categories.length + 1
-        // categories.push(args)
-        // return args
         return CategoryModel.create(args)
       }
     },
@@ -136,9 +111,6 @@ const RootMutation = new GraphQLObjectType({
         }
       },
       resolve(parent, args){
-        // args.id = products.length + 1
-        // products.push(args)
-        // return args
         return ProductModel.create(args)
       }
     }
