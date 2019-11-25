795cd64cc3d7d71366ea6ddd7a0ecfdb6fb8caae
zhangyanling77 <18783226594@163.com>
2019-11-25 17:28:02 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,13 +9,14 @@ import { Product } from './types'
 
 function App() {
   let [product, setProduct] = useState<Product>()
-  const { loading, error, data } = useQuery(CATEGORIES_PRODUCTS)
-  if(error){
-    return <p>加载发生错误</p>
-  }
-  if(loading){
-    return <p>加载中...</p>
-  }
+  const { loading, error, data, refetch, networkStatus } = useQuery(CATEGORIES_PRODUCTS, {
+    // skip: !name, // 当某个参数不存在时，跳过该请求
+    // pollInterval: 500, // 轮询时间
+    notifyOnNetworkStatusChange: true
+  })
+  if(networkStatus === 4) return <p>重新加载中...</p>
+  if(error) return <p>加载发生错误</p>
+  if(loading) return <p>加载中...</p>
   // console.log(data) // getCategories,getProducts两个list
   const { getCategories, getProducts } = data
 
@@ -25,7 +26,7 @@ function App() {
         <div className="container-fluid">
           <div className="navbar-header">
             {/* <a className="navbar-brand" href="#">首页</a> */}
-            <a href="#" className="navbar-brand">
+            <a href="#" className="navbar-brand" onClick={() => refetch()}>
               <img alt="header" style={{width: 24, height: 24}} 
               src="https://mirror-gold-cdn.xitu.io/168e08be61400b23518?imageView2/1/w/180/h/180/q/85/format/webp/interlace/1"></img>
             </a>
