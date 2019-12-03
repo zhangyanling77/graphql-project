4a2a7a4883e64f38b84b77ef037fe8f82a84d5bd
zhangyanling77 <18783226594@163.com>
2019-12-03 14:35:42 +0800

    Update ProductList.tsx

--- client/src/ProductList.tsx
+++ client/src/ProductList.tsx
@@ -9,27 +9,40 @@ interface Props {
 function ProductList(props: Props){
   if(!props.products) return <p>暂无数据</p>
   return (
-    <table className="table table-striped">
-      <caption className="text-center">产品列表</caption>
-      <thead>
-        <tr>
-          <td>名称</td>
-          <td>分类</td>
-          <td>操作</td>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          props.products.map((item: Product, index:number) => (
-            <tr key={item.id} onClick={() => props.setProduct(item)}>
-              <td>{item.name}</td>
-              <td>{item.category!.name}</td>
-              <td><a href="#">修改</a> | <a href="#" style={{color: 'red'}}>删除</a></td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
+    <>
+      <table className="table table-striped">
+        <caption className="text-center">产品列表</caption>
+        <thead>
+          <tr>
+            <td>名称</td>
+            <td>分类</td>
+            <td>操作</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            props.products.map((item: Product, index:number) => (
+              <tr key={item.id} onClick={() => props.setProduct(item)}>
+                <td>{item.name}</td>
+                <td>{item.category!.name}</td>
+                <td><a href="#">修改</a> | <a href="#" style={{color: 'red'}}>删除</a></td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+     <ul className="pagination">
+        <li className="page-item disabled">
+          <a className="page-link" href="#" aria-disabled="true">Previous</a>
+        </li>
+        <li className="page-item"><a className="page-link" href="#">1</a></li>
+        <li className="page-item"><a className="page-link" href="#">2</a></li>
+        <li className="page-item"><a className="page-link" href="#">3</a></li>
+        <li className="page-item">
+          <a className="page-link" href="#">Next</a>
+        </li>
+      </ul>
+    </>
   )
 }
 
