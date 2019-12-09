83f7beb1a55452a390aa5679f3590aeb52643236
zhangyanling77 <18783226594@163.com>
2019-12-09 14:45:08 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const Koa = require('koa')
 const mount = require('koa-mount')
 const graphqlHTTP = require('koa-graphql')
-const Router = require('koa-router')
+// const Router = require('koa-router')
 const cors = require('koa2-cors') // 解决跨域
 const logger = require('koa-logger')
 const myGraphQLSchema = require('./schema')
 
 const app = new Koa()
-const router = new Router()
+// const router = new Router()
 
 app.use(logger())
 
@@ -16,17 +16,17 @@ app.use(cors({
   allowMethods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'] // 允许的方法名
 }))
 
-// app.use(mount('/graphql', graphqlHTTP({
-//   schema: myGraphQLSchema,
-//   graphiql: true // 开启graphiql可视化操作ide
-// })))
-
-router.all('/graphql', graphqlHTTP({
+app.use(mount('/graphql', graphqlHTTP({
   schema: myGraphQLSchema,
-  graphiql: true
-}))
+  graphiql: true // 开启graphiql可视化操作ide
+})))
 
-app.use(router.routes()).use(router.allowedMethods())
+// router.all('/graphql', graphqlHTTP({
+//   schema: myGraphQLSchema,
+//   graphiql: true
+// }))
+
+// app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(4000, () => {
   console.log('server started on 4000')
